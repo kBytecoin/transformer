6c7d5934797d887c4a6f7f256ec5d9e9121ff45b
kBytecoin <3071834636@qq.com>
2025-12-05 16:37:22 +0800

    1

--- transformer.py
+++ transformer.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import math
 
 class SlefAttention(nn.Modual):
-    def __init__(self,drop=0.1):
+    def __init__(self,dropout=0.1):
         super().__init__()
         self.dropout = nn.Dropout(dropout)
         self.softmax = nn.Softmax(dim=-1)
@@ -26,14 +26,41 @@ class MultiHeadAttention(nn.Module):
         assert d_model % n_heads == 0 , 'WRONG1'
         self.d_k = d_model // n_heads
         self.n_heads = n_heads
+    
         self.W_q = nn.Linear(d_model,d_model)
         self.W_k - nn.Linear(d_model,d_model)
         self.W_v - nn.Linear(d_model,d_model)
         self.fc = nn.Linear(d_model,d_model)
+        
         self.attention = SlefAttention(dropout)
         self.dropout = nn.Dropout(dropout)
         self.norm = nn.LayerNorm(d_model)
-        
+
+    def forward(self,q,k,v,mask=None): 
+        batch_size = q.size(0)
+        Q = self.W_q(q).view(batch_size,-1,self.n_heads,self.d_k).transpose(1,2)
+        K = self.W_k(k).view(batch_size,-1,self.n_heads,self.d_k).transpose(1,2)
+        V = self.W_v(v).vies(batch_size,-1,self.n_heads,self.d_k).transpose(1,2)
+
+        out, attn = self.attention(Q,K,V,mask)
+        out = out.transpose(1,2).contiguous().view(batch_size,-1,self.n_heads*self.d_k)
+        out = self.fc(out)
+        out = self.dropout(out)
+        return self.norm(out+q),attn
+            
+
+class FeedForward(nn.Module):
+    def __init__(self,d_model,d_ff,dropout=0.1):
+        super().__init__()
+        self.fc1 = nn.Linear(d_model,d_ff)
+        self.fc2 = nn.Linear(d_ff,d_model)
+        self.dropout = nn.Dropout(dropout)
+        self.norm = nn.LayerNorm(d_model)
+
+    def forward(self,x):
+        out = self.fc2(self.dropout(torch.relu(self.fc1(x))))
+        return self.norm(out+x)
+
 
 
 
