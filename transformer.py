742dc37bff24ef9ac3ee21e4f740105988d17504
kBytecoin <3071834636@qq.com>
2025-12-05 16:37:16 +0800

    1

--- transformer.py
+++ transformer.py
@@ -24,6 +24,17 @@ class MultiHeadAttention(nn.Module):
     def __init__(self,d_model,n_heads,dropout=0.1):
         super().__init__()
         assert d_model % n_heads == 0 , 'WRONG1'
+        self.d_k = d_model // n_heads
+        self.n_heads = n_heads
+        self.W_q = nn.Linear(d_model,d_model)
+        self.W_k - nn.Linear(d_model,d_model)
+        self.W_v - nn.Linear(d_model,d_model)
+        self.fc = nn.Linear(d_model,d_model)
+        self.attention = SlefAttention(dropout)
+        self.dropout = nn.Dropout(dropout)
+        self.norm = nn.LayerNorm(d_model)
+        
+
 
 
         
